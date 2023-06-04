21cca3f41fd912ec97b69c785f5d497c0d350481
Shivam-Dhamesha <shivamdhamesha1@gmail.com>
2023-06-04 10:21:13 +0000

    Add an example of python named example.py

---
+++ example.py
@@ -0,0 +1,5 @@
+def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation()
+
+#this code was not written by Shivam Dhamesha. Assign by the instructor for the same.
